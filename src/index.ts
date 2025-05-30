16a0d325b35a038b3767aea42f745b99cd2031a6
Sean Coughlin <coughlinscc33@gmail.com>
2025-05-29 21:13:08 -0500

    Add static serving

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+const PORT = 8080;
 
-app.listen(8080, () => {
-  console.log("Server is running on port 8080");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static("."));
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
