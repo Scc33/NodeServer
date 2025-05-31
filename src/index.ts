1028f773afbb49fe8d072424d4a448d21d8e2ea1
Sean Coughlin <coughlinscc33@gmail.com>
2025-05-30 19:54:57 -0500

    Remove index.html and logo asset; enhance server with middleware for logging responses and health check endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,33 @@
 import express from "express";
+import type { Request, Response, NextFunction } from "express";
 
 const app = express();
 const PORT = 8080;
 
-app.use(express.static("src"));
+const middlewareLogResponses = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  res.on("finish", () => {
+    if (res.statusCode !== 200) {
+      console.log(
+        `[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`
+      );
+    }
+  });
+  next();
+};
+
+app.use(middlewareLogResponses);
+
+app.use("/app", express.static("./src/app"));
+
+const handlerReadiness = (req: Request, res: Response) => {
+  res.status(200).contentType("text/plain; charset=utf-8").send("OK");
+};
+
+app.get("/healthz", handlerReadiness);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
