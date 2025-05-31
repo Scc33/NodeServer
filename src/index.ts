4c991f3727b0d20ed364897bef8dbc0d88a5fc28
Sean Coughlin <coughlinscc33@gmail.com>
2025-05-30 19:43:14 -0500

    Update static file serving to use 'src' directory and add logo asset

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,10 @@
 import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
 
 const app = express();
 const PORT = 8080;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use(express.static("."));
+app.use(express.static("src"));
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
-});
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
 });
